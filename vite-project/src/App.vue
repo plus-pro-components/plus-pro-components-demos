9a3f574e66ff21e942856836133965b8b84a3824
xiaofei <lixiaofei0627@gmail.com>
2023-10-18 12:58:04 +0800

    init

---
+++ vite-project/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <el-config-provider :locale="locale">
+    <!-- 自动导入 -->
+    <PlusDatePicker v-model="value" />
+    {{ value }}
+  </el-config-provider>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import plusZhCn from 'plus-pro-components/locale/zh-cn'
+
+const value = ref()
+const locale = ref({ ...zhCn, ...plusZhCn })
+</script>
