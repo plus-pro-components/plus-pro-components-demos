75266991c09fb4a1c739c29eee071182de9fa7bd
xiaofei <lixiaofei0627@gmail.com>
2024-06-06 17:27:59 +0800

    update dependencies

--- vue-cli-project/src/main.js
+++ vue-cli-project/src/main.js
@@ -1,8 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-// import s from 'plus-pro-components/es/components/date-picker/style/css'
-
-// console.log(s)
-
 createApp(App).mount('#app')
